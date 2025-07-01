b971c373f0ce16a9cbab5119cf60dde45ae0e061
Shahriyar434 <rehimlitofiq9@gmail.com>
2025-07-01 12:19:00 +0400

    hw2
    bank cart

--- shahriyars_hw/src/mygame/heroes/polyphorsimsm.java
+++ shahriyars_hw/src/mygame/heroes/polyphorsimsm.java
@@ -1,35 +1,78 @@
 package mygame.heroes;
 
 public class polyphorsimsm {
-    public static void main(String[] args){
-        Dog dog = new Dog();
-        Cat cat = new Cat();
-        Animal also_dog = new Dog();
+    public static void main(String[] args) {
+        BankAccount[] accounts = new BankAccount[3];
 
-        dog.sound();
-        cat.sound();
-        also_dog.sound();
+        accounts[0] = new SavingsAccount();
+        accounts[1] = new CheckingAccount();
+        accounts[2] = new BusinessAccount();
 
+        for (BankAccount acc : accounts) {
+            acc.accountType();
+            acc.deposit(1000);
+            acc.withdraw(500);
+            System.out.println("Баланс: " + acc.balance);
+            System.out.println();
+        }
     }
 }
 
-class Animal {
-    void sound(){
 
+class BankAccount {
+    double balance;
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public void accountType() {
+        System.out.println("Generic account");
     }
 }
 
-class Dog extends Animal{
+
+class SavingsAccount extends BankAccount {
     @Override
-    void sound(){
-        System.out.println("dog's sound");
+    public void withdraw(double amount) {
+        System.out.println("Buy something from the cart");
+        super.withdraw(amount);
+    }
+
+    @Override
+    public void accountType() {
+        System.out.println("Save balance");
     }
 }
 
-class Cat extends Animal{
+
+class CheckingAccount extends BankAccount {
     @Override
-    void sound(){
-        System.out.println("cat's sound");
+    public void withdraw(double amount) {
+        System.out.println("Spent money");
+        super.withdraw(amount);
     }
 
+    @Override
+    public void accountType() {
+        System.out.println("Current account");
+    }
+}
+
+
+class BusinessAccount extends BankAccount {
+    @Override
+    public void withdraw(double amount) {
+        System.out.println("Spend money from business account");
+        super.withdraw(amount);
+    }
+
+    @Override
+    public void accountType() {
+        System.out.println("Business account");
+    }
 }
