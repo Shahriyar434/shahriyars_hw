9a091e967e873da3220960672b21df9a7987f89d
Shahriyar434 <rehimlitofiq9@gmail.com>
2025-09-14 13:28:48 +0400

    instructor

---
+++ shahriyars_hw/src/mygame/heroes/Instructor.java
@@ -0,0 +1,41 @@
+package mygame.heroes;
+
+import java.io.File;
+
+class Instuctor {
+    public static void main(String[] args) {
+
+        File mylife = new File("C:\\Users\\asus\\Desktop\\My life");
+
+
+        File myday = new File(mylife, "My day");
+
+
+        System.out.println("Is 'My life' a directory? " + mylife.isDirectory());
+        System.out.println("Is 'My day' a directory? " + myday.isDirectory());
+
+
+        System.out.println("Absolute path (My life): " + mylife.getAbsolutePath());
+        System.out.println("Absolute path (My day): " + myday.getAbsolutePath());
+
+
+        System.out.println("'My life' exists: " + mylife.exists());
+        System.out.println("'My day' exists: " + myday.exists());
+
+
+        if (!mylife.exists()) {
+            
+            System.out.println("'My life' folder created.");
+        }
+        if (!myday.exists()) {
+
+            System.out.println("'My day' folder created.");
+        }
+
+
+        System.out.println("Delete 'My day': " + myday.delete());
+
+
+        System.out.println("'My day' exists after delete: " + myday.exists());
+    }
+}
